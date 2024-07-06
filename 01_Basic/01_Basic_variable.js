6ff8efd479e5a3baf027f9e5582de7bfadd8a32e
Vikash kumar <141928908+Vikash-Yadav123@users.noreply.github.com>
2024-07-06 12:38:04 +0000

    add variable files

---
+++ 01_Basic/01_Basic_variable.js
@@ -0,0 +1,16 @@
+const accountId=1233333;
+let accountName="Vikash";
+var accountCity="patna";
+accountAge=19;
+ 
+// accountId=133;
+accountName="vinay";
+accountCity="delhi";
+accountAge=20;
+// console.log(accountId);
+console.table([accountId,accountName,accountAge,accountCity]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
