2cb847c09b65133952affdfad5b2eb78cde4feef
Vikash kumar <141928908+Vikash-Yadav123@users.noreply.github.com>
2024-07-07 13:35:31 +0000

    add 03_conversionOperation

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,60 @@
+let score=false;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let numberValue=Number(score);
+// console.log(numberValue);
+// console.log(typeof numberValue);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+
+let isLoggedIn=null;
+let BooleanValue=Boolean(isLoggedIn);
+// console.log(BooleanValue);
+// console.log(typeof BooleanValue);
+
+// "vikasj"=>true;
+// ""=>false;
+// 0=>false;
+//1=>true;
+// null=>false;
+
+let number=false;
+let StringValue=String(number);
+// console.log(StringValue);
+// console.log(typeof StringValue);
+
+//*********************OPERATION */
+
+let value=3;
+let negativeValue=-value;
+// console.log(negativeValue);
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3/3);
+// console.log(3**3);
+// console.log(3%3);
+
+// let str1="vikash";
+// let str2=" kumar";
+// let str=str1+str2;
+// console.log(str);
+
+// console.log(1+2+3);
+// console.log("1"+2+3);
+// console.log(1+2+"2");
+// console.log((2+3)*5/3);
+
+// console.log(+true);
+// console.log(+"");
+let num1,num2,num3;
+// num1=num2=num3=2+2
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter)
