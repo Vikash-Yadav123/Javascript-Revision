881f56d813e8e53bf2da2ebdd51d6cadb2d862f2
Vikash kumar <141928908+Vikash-Yadav123@users.noreply.github.com>
2024-07-25 11:37:05 +0000

    number

---
+++ 01_Basic/07_nums_maths.js
@@ -0,0 +1,10 @@
+const score=100;
+// console.log(score);
+
+const number=new Number(100);
+// console.log(number);
+// console.log(number.toString());
+const otherNumber=23.8945;
+// console.log(otherNumber.toFixed(1));
+// console.log(otherNumber.toLocaleString('en-IN'));
+console.log(otherNumber.toPrecision(2));
