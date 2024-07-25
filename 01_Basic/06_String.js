30c95ee214a0b8e566da486cf950a9649c1c44d2
Vikash kumar <141928908+Vikash-Yadav123@users.noreply.github.com>
2024-07-25 10:56:08 +0000

    string all method practice

---
+++ 01_Basic/06_String.js
@@ -0,0 +1,28 @@
+const name="VIKASH";
+const githubProfileName="vikashyadav";
+// console.log(name+githubProfileName);
+// console.log(`my name is ${name} and githubprofile name is ${githubProfileName}`);
+
+const facebookProfileName=new String('Vikash');
+const array1=["v","i","k","a","s","h"];
+const array2=["k","u","m","a","r"];
+// const array3=array1.concat(array2);
+// console.log(array3);
+// console.log(array1.includes('i'));
+// console.log(array1.indexOf("i"));
+const para="  my name is anil kumar anil ";
+// console.log(para.replace("anil","vikash"))
+// console.log(para.replaceAll("anil","vikash"));
+// console.log(para.search('kumar'));
+// console.log(array1);
+// console.log(array1.slice(1,6));
+// console.log(facebookProfileName.toLowerCase());
+// console.log(facebookProfileName.toUpperCase());
+// const d=new Date();
+// console.log(d.toString());
+// console.log(d);
+// console.log(para);
+// console.log(para.trim());
+// console.log(para.trimStart());
+// console.log(para.trimEnd());
+
