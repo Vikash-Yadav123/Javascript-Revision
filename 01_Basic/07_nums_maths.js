2c8c0a2d141311837e5ea497744364db5e915568
Vikash kumar <141928908+Vikash-Yadav123@users.noreply.github.com>
2024-07-26 11:13:42 +0000

    maths

--- 01_Basic/07_nums_maths.js
+++ 01_Basic/07_nums_maths.js
@@ -7,4 +7,23 @@ const number=new Number(100);
 const otherNumber=23.8945;
 // console.log(otherNumber.toFixed(1));
 // console.log(otherNumber.toLocaleString('en-IN'));
-console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(2));
+
+// ++++++++++++++++++++++++MATH++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4,5,6));
+// console.log(Math.max(4,5,6));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+min=10;
+max=20;
+// console.log(Math.floor(Math.random()*(max-min+1))+min); important
